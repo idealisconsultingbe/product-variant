7fc7ce174ec2e898032ec6224091c61e7e4e1920
Idealis Consulting <info@idealisconsulting.com>
2019-01-18 08:39:21 +0000

    EKI2-8 ACH2: debug plusieurs produits sur so lines

--- project_addons/eki_purchase/models/eki_purchase.py
+++ project_addons/eki_purchase/models/eki_purchase.py
@@ -34,18 +34,20 @@ class EkiPurchaseOrder(models.Model):
         if self.partner_id:
             now = fields.Datetime.now()
             values = []
+            product_in_values = []
             supplier_infos = self.env['product.supplierinfo'].search([('name', '=', self.partner_id.id), '|', '&', ('date_start', '<=', now), ('date_end', '>=', now), '&', ('date_start', '=', False), ('date_end', '=', False)])
             for supplier_info in supplier_infos:
                 product = supplier_info.product_id if supplier_info.product_id else supplier_info.product_tmpl_id.product_variant_id
-                if product:
-                    values.append((0,0,{
+                if product and product.id not in product_in_values:
+                    product_in_values.append(product.id)
+                    values.append({
                             'name': product.name,
                             'product_id': product.id,
                             'product_qty': 0,
                             'product_uom': supplier_info.product_uom.id,
                             'price_unit': product.list_price,
                             'date_planned': now,
-                        }))
+                        })
             self.update({'order_line': values})
 
 
